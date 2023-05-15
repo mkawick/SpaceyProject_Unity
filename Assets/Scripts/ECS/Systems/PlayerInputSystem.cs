3dec2824d05db072852a1408cff57889598113eb
MickeyQuell <77269450+MickeyQuell@users.noreply.github.com>
2023-05-15 01:34:48 +0100

    Setting up physics layers

--- Assets/Scripts/ECS/Systems/PlayerInputSystem.cs
+++ Assets/Scripts/ECS/Systems/PlayerInputSystem.cs
@@ -8,8 +8,8 @@ public partial class PlayerInputSystem : SystemBase
     {
         Entities.ForEach( (ref MoveControllerData movement, ref AttackControllerData acd, in UserInput userInput) =>
         {
-            if (Input.GetKey(userInput.left)) { movement.direction.x = 1; }
-            else if (Input.GetKey(userInput.right)) { movement.direction.x = -1; }
+            if (Input.GetKey(userInput.left)) { movement.direction.x = -1; }
+            else if (Input.GetKey(userInput.right)) { movement.direction.x = 1; }
             else movement.direction.x = 0;
 
             if (Input.GetKey(userInput.up)) { movement.direction.z = -1; }
